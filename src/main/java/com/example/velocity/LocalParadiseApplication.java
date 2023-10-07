0af3b45bb89cf20c933755419dca81a3206b0b6a
madhurikurhe123 <madhuriunde2408@gmail.com>
2023-10-07 12:48:38 +0530

    Test Commit

---
+++ src/main/java/com/example/velocity/LocalParadiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalParadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalParadiseApplication.class, args);
+	}
+
+}
